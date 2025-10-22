a1a7908bbfa7d6c68aa871725c55a67dc818f41d
yironglin <yeerolin11@gmail.com>
2025-10-21 19:30:19 -0700

    experiment setting

--- cbramod_finetune/finetune_custom_stress.sh
+++ cbramod_finetune/finetune_custom_stress.sh
@@ -1,9 +1,11 @@
+# Stress Dataset: CBraMod default classifier, 3 layers
 python finetune_main.py \
 --downstream_dataset CustomStress \
 --datasets_dir /work/HHRI-AI/YW/Yirong/LaBramFinetune/augmented_data/Stress_noleak_30chan_no400up_seed_siwen42 \
 --num_of_classes 2 \
 --model_dir /work/HHRI-AI/YW/Yirong/CBraMod/models_weights/finetune-Stress_noleak_30chan_no400up_seed_siwen42-4review-default-all_reps
 
+# Stress Dataset: CBraMod, default classifier, avgpool reps
 python finetune_main.py \
 --downstream_dataset CustomStress \
 --datasets_dir /work/HHRI-AI/YW/Yirong/LaBramFinetune/augmented_data/Stress_noleak_30chan_no400up_seed_siwen42 \
@@ -11,6 +13,7 @@ python finetune_main.py \
 --classifier avgpooling_patch_reps \
 --model_dir /work/HHRI-AI/YW/Yirong/CBraMod/models_weights/finetune-Stress_noleak_30chan_no400up_seed_siwen42-4review-default-avgpooling_reps 
 
+# Stress Dataset: CBraMod, default classifier, onelayer
 python finetune_main.py \
 --downstream_dataset CustomStress \
 --datasets_dir /work/HHRI-AI/YW/Yirong/LaBramFinetune/augmented_data/Stress_noleak_30chan_no400up_seed_siwen42 \
@@ -18,7 +21,7 @@ python finetune_main.py \
 --classifier all_patch_reps_onelayer \
 --model_dir /work/HHRI-AI/YW/Yirong/CBraMod/models_weights/finetune-Stress_noleak_30chan_no400up_seed_siwen42-4review-default-reps_onelayer 
 
-
+# Stress Dataset: CBraMod, Labram classifier,
 python finetune_main.py \
 --downstream_dataset CustomStress \
 --datasets_dir /work/HHRI-AI/YW/Yirong/LaBramFinetune/augmented_data/Stress_noleak_30chan_no400up_seed_siwen42 \
@@ -26,7 +29,7 @@ python finetune_main.py \
 --classifier Labram_style_classifier \
 --model_dir /work/HHRI-AI/YW/Yirong/CBraMod/models_weights/finetune-Stress_noleak_30chan_no400up_seed_siwen42-4review-Labram_style_classifier 
 
-
+# Stress Dataset: CBraMod, Labram classifier2,
 python finetune_main.py \
 --downstream_dataset CustomStress \
 --datasets_dir /work/HHRI-AI/YW/Yirong/LaBramFinetune/augmented_data/Stress_noleak_30chan_no400up_seed_siwen42 \
