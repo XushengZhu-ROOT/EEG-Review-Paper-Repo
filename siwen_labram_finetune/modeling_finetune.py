458df69b00563482046e391eb224a4f6a0b98720
yironglin <yeerolin11@gmail.com>
2025-10-22 02:43:25 -0700

    update cbramod style classifier (3 layer) experiment and result

--- siwen_labram_finetune/modeling_finetune.py
+++ siwen_labram_finetune/modeling_finetune.py
@@ -16,6 +16,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from timm.models.layers import drop_path, to_2tuple, trunc_normal_
 from timm.models.registry import register_model
+from einops.layers.torch import Rearrange
 from einops import rearrange
 
 
@@ -462,12 +463,248 @@ class NeuralTransformer(nn.Module):
 
         return features
 
+class BinaryClassifierHead_CbramodStyle_3Ly_BCE(nn.Module):
+    def __init__(self, embed_dim=200, num_patches=150, dropout=0.5, init_scale=0.001):
+        super().__init__()
+        input_dim = num_patches * embed_dim # 30000
+        
+        # 定義三層 MLP
+        self.fc = nn.Sequential(
+            Rearrange('b c d -> b (c d)'), # (b, 150, 200)
+            nn.Linear(input_dim, 5 * embed_dim), # Layer 1: 30000 -> 1000
+            nn.ELU(),
+            nn.Dropout(dropout),
+            nn.Linear(5 * embed_dim, embed_dim), # Layer 2: 1000 -> 200
+            nn.ELU(),
+            nn.Dropout(dropout),
+            nn.Linear(embed_dim, 1), # Layer 3: 200 -> 1 (統一輸出 1 維 Logit)
+        )
+
+        if init_scale != 1.0:
+            final_linear = self.fc[-1] # 取得 nn.Linear(embed_dim, 1)
+            if isinstance(final_linear, nn.Linear):
+                 final_linear.weight.data.mul_(init_scale)
+                 if final_linear.bias is not None:
+                     final_linear.bias.data.mul_(init_scale)
+
+    def forward(self, x):
+        return self.fc(x)
+
+class NeuralTransformer_Cbramod3lyClassifier_BCE(nn.Module):
+    def __init__(self, EEG_size=1600, patch_size=200, in_chans=1, out_chans=8, num_classes=1000, embed_dim=200, depth=12,
+                 num_heads=10, mlp_ratio=4., qkv_bias=False, qk_norm=None, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
+                 drop_path_rate=0., norm_layer=nn.LayerNorm, init_values=None,
+                 use_abs_pos_emb=True, use_rel_pos_bias=False, use_shared_rel_pos_bias=False,
+                 use_mean_pooling=True, init_scale=0.001, classifier_dropout=0.1, **kwargs):
+        super().__init__()
+        self.num_classes = num_classes
+        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
+
+        # To identify whether it is neural tokenizer or neural decoder. 
+        # For the neural decoder, use linear projection (PatchEmbed) to project codebook dimension to hidden dimension.
+        # Otherwise, use TemporalConv to extract temporal features from EEG signals.
+        self.patch_embed = TemporalConv(out_chans=out_chans) if in_chans == 1 else PatchEmbed(EEG_size=EEG_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
+        self.time_window = EEG_size // patch_size
+        self.patch_size = patch_size
+        self.classifier_dropout = classifier_dropout
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        # self.mask_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        if use_abs_pos_emb:
+            self.pos_embed = nn.Parameter(torch.zeros(1, 128 + 1, embed_dim), requires_grad=True)
+        else:
+            self.pos_embed = None
+        self.time_embed = nn.Parameter(torch.zeros(1, 16, embed_dim), requires_grad=True)
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        self.rel_pos_bias = None
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
+        self.use_rel_pos_bias = use_rel_pos_bias
+        self.blocks = nn.ModuleList([
+            Block(
+                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_norm=qk_norm, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer,
+                init_values=init_values, window_size=None)
+            for i in range(depth)])
+        self.norm = nn.Identity() if use_mean_pooling else norm_layer(embed_dim)
+        self.fc_norm = norm_layer(embed_dim) if use_mean_pooling else None
+        # self.head = nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+        self.head = BinaryClassifierHead_CbramodStyle_3Ly_BCE(embed_dim=embed_dim, num_patches=150, dropout=self.classifier_dropout) if num_classes > 0 else nn.Identity() # 150 = 30 channel * 5 seconds
+
+        if self.pos_embed is not None:
+            trunc_normal_(self.pos_embed, std=.02)
+        if self.time_embed is not None:
+            trunc_normal_(self.time_embed, std=.02)
+        trunc_normal_(self.cls_token, std=.02)
+        # trunc_normal_(self.mask_token, std=.02)
+        # if isinstance(self.head, nn.Linear):
+        #     trunc_normal_(self.head.weight, std=.02)
+        self.apply(self._init_weights)
+        self.fix_init_weight()
+
+    def fix_init_weight(self):
+        def rescale(param, layer_id):
+            param.div_(math.sqrt(2.0 * layer_id))
+
+        for layer_id, layer in enumerate(self.blocks):
+            rescale(layer.attn.proj.weight.data, layer_id + 1)
+            rescale(layer.mlp.fc2.weight.data, layer_id + 1)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def get_num_layers(self):
+        return len(self.blocks)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'pos_embed', 'cls_token', 'time_embed'}
+
+    def get_classifier(self):
+        return self.head
+
+    def reset_classifier(self, num_classes, global_pool=''):
+        self.num_classes = num_classes
+        # self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+        self.head = BinaryClassifierHead_CbramodStyle_3Ly_BCE(embed_dim=embed_dim, num_patches=150, dropout=self.classifier_dropout) if num_classes > 0 else nn.Identity() 
+
+    def forward_features(self, x, input_chans=None, return_patch_tokens=False, return_all_tokens=False, **kwargs):
+        batch_size, n, a, t = x.shape
+        input_time_window = a if t == self.patch_size else t
+        x = self.patch_embed(x)
+
+        cls_tokens = self.cls_token.expand(batch_size, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+
+        x = torch.cat((cls_tokens, x), dim=1)
+
+        pos_embed_used = self.pos_embed[:, input_chans] if input_chans is not None else self.pos_embed
+        if self.pos_embed is not None:
+            pos_embed = pos_embed_used[:, 1:, :].unsqueeze(2).expand(batch_size, -1, input_time_window, -1).flatten(1, 2)
+            pos_embed = torch.cat((pos_embed_used[:,0:1,:].expand(batch_size, -1, -1), pos_embed), dim=1)
+            x = x + pos_embed
+        if self.time_embed is not None:
+            nc = n if t == self.patch_size else a
+            time_embed = self.time_embed[:, 0:input_time_window, :].unsqueeze(1).expand(batch_size, nc, -1, -1).flatten(1, 2)
+            x[:, 1:, :] += time_embed
+
+        x = self.pos_drop(x)
+        
+        for blk in self.blocks:
+            x = blk(x, rel_pos_bias=None)
+        
+        x = self.norm(x)
+        if self.fc_norm is not None:
+            if return_all_tokens:
+                return self.fc_norm(x)
+            t = x[:, 1:, :]
+            if return_patch_tokens:
+                return self.fc_norm(t)
+            else:
+                return self.fc_norm(t.mean(1))
+        else:
+            if return_all_tokens:
+                return x
+            elif return_patch_tokens:
+                return x[:, 1:]
+            else:
+                return x[:, 0]
+
+    def forward(self, x, input_chans=None, return_patch_tokens=False, return_all_tokens=False, **kwargs):
+        '''
+        x: [batch size, number of electrodes, number of patches, patch size]
+        For example, for an EEG sample of 4 seconds with 64 electrodes, x will be [batch size, 64, 4, 200]
+        '''
+        # x = self.forward_features(x, input_chans=input_chans, return_patch_tokens=return_patch_tokens, return_all_tokens=return_all_tokens, **kwargs)
+        x = self.forward_features(x, input_chans=input_chans, return_patch_tokens=True, return_all_tokens=False, **kwargs)
+        x = self.head(x)
+        return x
+
+    def forward_intermediate(self, x, layer_id=12, norm_output=False):
+        x = self.patch_embed(x)
+        batch_size, seq_len, _ = x.size()
+
+        cls_tokens = self.cls_token.expand(batch_size, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        x = torch.cat((cls_tokens, x), dim=1)
+        if self.pos_embed is not None:
+            pos_embed = self.pos_embed[:, 1:, :].unsqueeze(2).expand(batch_size, -1, self.time_window, -1).flatten(1, 2)
+            pos_embed = torch.cat((self.pos_embed[:,0:1,:].expand(batch_size, -1, -1), pos_embed), dim=1)
+            x = x + pos_embed
+        if self.time_embed is not None:
+            time_embed = self.time_embed.unsqueeze(1).expand(batch_size, 62, -1, -1).flatten(1, 2)
+            x[:, 1:, :] += time_embed
+        x = self.pos_drop(x)
+
+        rel_pos_bias = self.rel_pos_bias() if self.rel_pos_bias is not None else None
+        if isinstance(layer_id, list):
+            output_list = []
+            for l, blk in enumerate(self.blocks):
+                x = blk(x, rel_pos_bias=rel_pos_bias)
+                # use last norm for all intermediate layers
+                if l in layer_id:
+                    if norm_output:
+                        x_norm = self.fc_norm(self.norm(x[:, 1:]))
+                        output_list.append(x_norm)
+                    else:
+                        output_list.append(x[:, 1:])
+            return output_list
+        elif isinstance(layer_id, int):
+            for l, blk in enumerate(self.blocks):
+                if l < layer_id:
+                    x = blk(x, rel_pos_bias=rel_pos_bias)
+                elif l == layer_id:
+                    x = blk.norm1(x)
+                else:
+                    break
+            return x[:, 1:]
+        else:
+            raise NotImplementedError(f"Not support for layer id is {layer_id} now!")
+    
+    def get_intermediate_layers(self, x, use_last_norm=False):
+        x = self.patch_embed(x)
+        batch_size, seq_len, _ = x.size()
+
+        cls_tokens = self.cls_token.expand(batch_size, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        x = torch.cat((cls_tokens, x), dim=1)
+        if self.pos_embed is not None:
+            pos_embed = self.pos_embed[:, 1:, :].unsqueeze(2).expand(batch_size, -1, self.time_window, -1).flatten(1, 2)
+            pos_embed = torch.cat((self.pos_embed[:,0:1,:].expand(batch_size, -1, -1), pos_embed), dim=1)
+            x = x + pos_embed
+        if self.time_embed is not None:
+            time_embed = self.time_embed.unsqueeze(1).expand(batch_size, 62, -1, -1).flatten(1, 2)
+            x[:, 1:, :] += time_embed
+        x = self.pos_drop(x)
+
+        features = []
+        rel_pos_bias = self.rel_pos_bias() if self.rel_pos_bias is not None else None
+        for blk in self.blocks:
+            x = blk(x, rel_pos_bias)
+            if use_last_norm:
+                features.append(self.norm(x))
+            else:
+                features.append(x)
+
+        return features
 
 @register_model
 def labram_base_patch200_200(pretrained=False, **kwargs):
     model = NeuralTransformer(
         patch_size=200, embed_dim=200, depth=12, num_heads=10, mlp_ratio=4, qk_norm=partial(nn.LayerNorm, eps=1e-6), # qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    return model
+    
+@register_model
+def labram_base_patch200_200_cbramod3lyclassifier(pretrained=False, **kwargs):
+    model = NeuralTransformer_Cbramod3lyClassifier_BCE(
+        patch_size=200, embed_dim=200, depth=12, num_heads=10, mlp_ratio=4, qk_norm=partial(nn.LayerNorm, eps=1e-6), # qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), classifier_dropout=0.1, **kwargs)
     model.default_cfg = _cfg()
     return model
 
